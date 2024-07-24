0d4fae8c7e3e8a13414ef1ec231e8d335886cbc2
AKHIL-DyC <akhiljee666@gmail.com>
2024-07-25 01:11:16 +0530

    front-1

--- src/app/page.js
+++ src/app/page.js
@@ -1,113 +1,56 @@
+"use client"
 import Image from "next/image";
+import gsap from "gsap";
+import { useEffect } from "react";
+import { League_Spartan } from "next/font/google";
+import Navbar from "./component/Navbar";
+import Hoverer from "./component/Hoverer";
+import Hoverer2 from "./component/Hoverer2";
+
+const fontss = League_Spartan({
+  subsets: ['latin'],
+  weight: ['800']
+});
 
 export default function Home() {
+  
+  useEffect(() => {
+    const t1 = gsap.timeline();
+
+    t1.to('.one', { opacity: 1, ease: gsap.easeInOut });
+    t1.to('.two', { opacity: 1, ease: gsap.easeInOut });
+    t1.to('.three', { opacity: 1, ease: gsap.easeInOut });
+    t1.to('.brown-background', { height: 0, duration: 1, ease: gsap.easeInOut, delay: 1 });
+    t1.to('.white-background', { height: '100vh', duration: 1 }, '-=0.5');
+
+    return () => {
+      t1.kill();
+    };
+  }, []);
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <>
+      <div style={{ height: '100vh', width: '100vw', overflow: 'hidden', backgroundColor: 'black' }}>
+        <div className="brown-background" style={{ display: 'flex', width: '100vw', height: '100vh', position: 'relative', backgroundColor: '#fdf0d5', justifyContent: 'center', alignContent: 'center', overflow: 'hidden' }}>
+          <div className="text-content" style={{ height: '40vh', fontFamily: fontss, lineHeight: '1.2' }}>
+            <h2 className="one" style={{ fontSize: '10vw', opacity: '0' }}>the</h2>
+            <h2 className="two" style={{ fontSize: '10vw', opacity: '0' }}>thrift</h2>
+            <h2 className="three" style={{ fontSize: '10vw', opacity: '0' }}>Kada.</h2>
+          </div>
+        </div>
+        <div className="white-background" style={{ height: '100vh', width: '100vw', position: 'absolute', backgroundColor: '#fdf0d5', bottom: '100', display: 'flex', justifyContent: 'space-between', overflow: 'hidden', flexDirection: 'column' }}>
+          <Navbar />
+          <div style={{ width: '100vw', height: '90vh', display: 'flex', flexDirection: 'row', justifyContent: 'space-between', padding: '80px', alignContent: 'center' }}>
+            <Hoverer2 />
+            <Hoverer />
+          </div>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+      <style jsx global>{`
+        .text-content h2 {
+          font-family: ${fontss.style.fontFamily};
+        }
+      `}</style>
+    </>
   );
 }
